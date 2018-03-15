90c6a65f414201453fc6aeaff24ae495b80ecee3
adamy318 <ayu4@lsu.edu>
2018-03-15 18:35:19 -0500

    Update Users table
    added attributes: username, email, date_created
    probably more attribute to come

---
+++ db/migrate/20180315231903_add_data_to_users.rb
@@ -0,0 +1,7 @@
+class AddDataToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :username, :string
+    add_column :users, :email, :string
+    add_column :users, :date_created, :datetime
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180313031959) do
+ActiveRecord::Schema.define(version: 20180315231903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,9 @@ ActiveRecord::Schema.define(version: 20180313031959) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "email"
+    t.datetime "date_created"
   end
 
 end
