3d1f092f838e4881a7d4a10a105dfceb6ced399b
adamy318 <ayu4@lsu.edu>
2018-03-16 01:10:48 -0500

    Removed column and updated view for signup page
    removed date_created column because rails already created that column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180315231903) do
+ActiveRecord::Schema.define(version: 20180316060232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 20180315231903) do
     t.string "username"
     t.string "email"
     t.string "password_hash"
-    t.datetime "date_created"
   end
 
 end
