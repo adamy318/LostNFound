b9b91f8411893766a550905083fab206eea77b76
adamy318 <ayu4@lsu.edu>
2018-03-20 13:53:43 -0500

    Updated validation on emails and added database indices to some columns
    the indices were added to make it easier to determine which/how many of some username/email exist in database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_insert { self.email = email.downcase }
   validates :username, presence: true, length: { maximum: 32 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180316060232) do
+ActiveRecord::Schema.define(version: 20180320184852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 20180316060232) do
     t.datetime "updated_at", null: false
     t.string "username"
     t.string "email"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
