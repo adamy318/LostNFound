bf96e8625ddd9f5457e52a36affad375a1dd3b2b
Courtland Crouchet <CourtlandCthulhu@gmail.com>
2018-03-19 21:02:30 -0500

    Added create method and user params to ListingsController
    Also added a new and a create view for listings

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,2 +1,13 @@
 class ListingsController < ApplicationController
+  def new
+  end
+
+  def create
+    @listing = Listings.create(user_params)
+  end
+
+  def user_params
+  params.require(:listing).permit(:image)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'listings/new'
+
+  get 'listings/create'
+
   get 'static_pages/home'
 
   get 'static_pages/about'
