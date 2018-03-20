0075f3ee76139b142fb3ef5abbd2d9498c5a11cd
adamy318 <ayu4@lsu.edu>
2018-03-20 15:58:32 -0500

    Added BCrypt module for hashing and also added password salts and hashes columns to database
    made skeleton outline for dealing with hashing passwords in models/user.rb file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
+require 'bcrypt'
+
 class User < ApplicationRecord
-  before_insert { self.email = email.downcase }
+
+  include BCrypt
+  include SecureRandom
+
+  before_insert_email { self.email = email.downcase }
+  before_insert_username { self.username = username.downcase }
   validates :username, presence: true, length: { maximum: 32 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  validates 
 end
---
+++ db/migrate/20180320205452_add_password_has_and_salt_columns.rb
@@ -0,0 +1,6 @@
+class AddPasswordHasAndSaltColumns < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :password_salt, :string
+    add_column :users, :password_hash, :string
+  end
+end
