10867a122299a6a085e940c739c2f4a436f708a8
adamy318 <ayu4@lsu.edu>
2018-04-12 19:20:34 -0500

    Added controller logic for users
    also changed some routes
    
    Added base framework for layout of site
    
    also slightly modified user controller and fixed typo in routes.rb
    added form for user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
   
   def show
+    @user = User.find(params[:id])
   end
 
   def new
-    render 'new'
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
   def create
