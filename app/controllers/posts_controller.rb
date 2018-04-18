78ec4fc30e118b80812cf7f5c21750b6ee7a7563
Courtland Crouchet <CourtlandCthulhu@gmail.com>
2018-04-18 16:30:36 -0500

    Added the ability to edit posts
    Users can now edit posts to change the title, picture, or text

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,10 @@ class PostsController < ApplicationController
 
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
   def show
     @post = Post.find(params[:id])
   end
@@ -17,6 +21,15 @@ class PostsController < ApplicationController
 
     @post.save
     redirect_to @post
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
   end
 
   private def post_params
