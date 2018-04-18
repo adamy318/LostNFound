4bc5c7656dbd0f12f20b8ee68a9ceaf7dc19e34b
adamy318 <ayu4@lsu.edu>
2018-04-17 22:28:55 -0500

    Fixed logic for checking user input when signing up
    also rearranged some of the code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,11 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
     if @user.save
       redirect_to @user
     else
@@ -12,7 +17,10 @@ class UsersController < ApplicationController
     end
   end
 
-  def create
-  end
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end
 
 end
