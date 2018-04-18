e761296d07c3bcd01c3624555014a1a0fa095a95
Courtland Crouchet <CourtlandCthulhu@gmail.com>
2018-04-18 00:21:31 -0500

    Added ability to add pictures
    Now I just need to make it possible to view them; Also going to work on adding the index view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180320014630) do
+ActiveRecord::Schema.define(version: 20180416222428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,18 +24,15 @@ ActiveRecord::Schema.define(version: 20180320014630) do
     t.datetime "image_updated_at"
   end
 
-  create_table "lists", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "sessions", force: :cascade do |t|
