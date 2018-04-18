3cbfb5ec6cd04d44c1557f01f6b9a1073ae7f09e
Courtland Crouchet <CourtlandCthulhu@gmail.com>
2018-04-18 00:32:13 -0500

    Images Work!
    You can now post images in a post and view them by clicking on a post from the index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
-  
+
   def new
 
   end
@@ -20,6 +20,6 @@ class PostsController < ApplicationController
   end
 
   private def post_params
-    params.require(:post).permit(:title, :body)
+    params.require(:post).permit(:title, :body, :image)
   end
 end
