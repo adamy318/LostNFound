6e680d1717430865f0510da1805e54d4d82a4c6e
Courtland Crouchet <CourtlandCthulhu@gmail.com>
2018-04-18 21:55:49 -0500

    Added paths for the links in the navbar

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,4 +10,7 @@ class StaticPagesController < ApplicationController
 
   def login
   end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  
+
   root 'static_pages#home'
+
+  get '/contact', to: 'static_pages#contact'
 
   get '/about', to: 'static_pages#about'
 
