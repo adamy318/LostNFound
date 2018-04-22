0153c1bcc2296bf1ff1efc139ccec7830dd6d9cf
adamy318 <ayu4@lsu.edu>
2018-04-22 15:00:13 -0500

    Added routes to config/routes.rb login routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,15 @@ Rails.application.routes.draw do
 
   get '/about', to: 'static_pages#about'
 
-  get '/login', to: 'static_pages#login'
-
   get '/signup', to: 'users#new'
 
   post '/signup', to: 'users#create'
+
+  get '/login', to: 'sessions#new'
+
+  post '/login', to: 'sessions#create'
+
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users
 
