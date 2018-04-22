e71678047557573ac08d396a7d6f4265c1163fba
adamy318 <ayu4@lsu.edu>
2018-04-22 16:16:39 -0500

    Fully fleshed out the logic for logging users in
    added some comments to function and added a couple of functions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(username: params[:session][:username])
+    if user && authenticate(params[:session][:username], params[:session][:password] 
+      log_in user
+      redirect_to root_path
+    else
+      flash.now[:danger] = 'Invalid username/password combination'
+      render 'new'
+    end
   end
 
   def destroy
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,37 @@
+require 'pbkdf2'
+
 module SessionsHelper
+
+  include SecureRandom
+  
+  # authenticates user by taking the salt and hash associated with
+  # inputted user (if any), then hashing the inputted password
+  # then the newly calculated password hash is checked against the one
+  # stored in the database
+  # returns true if password hashes match, false otherwise
+  def authenticate(user, password)
+    salt = user.pass_salt
+
+    hash = PBKDF2.new do |p|
+      p.password = password.unpack("B*").first
+      p.salt = salt
+      p.iterations = 10000
+    end
+
+    pass_hash = hash.hex_string
+   
+    pass_hash == user.pass_hash 
+  end
+
+
+  # logs in user
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # gets current logged in user
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
 end
