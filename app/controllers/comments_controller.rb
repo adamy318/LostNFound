7ecabd881316441db78d66e726c7c9ffbbae6fb3
adamy318 <ayu4@lsu.edu>
2018-04-22 23:14:00 -0500

    Fixed a syntax error, comments can now be added but they're super ugly, so separating that from the initial screen or at least getting it smaller is the next step

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,4 +4,8 @@ class CommentsController < ApplicationController
     @comment = @post.comments.create(params[:comment])
     redirect_to post_path(@post)
   end
+
+  def comment_params
+    params.require(:body, :commenter, :post)
+  end
 end
