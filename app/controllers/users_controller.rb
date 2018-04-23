eaac0d16a83f1c1d2f039e718f90448ee0b07a8f
adamy318 <ayu4@lsu.edu>
2018-04-22 19:11:32 -0500

    Fixed users not being loged in right after they register
    added 1 line

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "You have successfully registered and are now logged in!"
       redirect_to root_path
     else
