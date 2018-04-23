ce1c76be3e6fd06fc2ecb2223375f0eab7880e56
Courtland <crouchetcourtland@gmail.com>
2018-04-22 22:55:24 -0500

    Added code so that posts are able to contain comments and comments are able to be parts of posts

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,8 @@ Rails.application.routes.draw do
 
   resources :users
 
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
